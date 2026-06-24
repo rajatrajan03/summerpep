5162f888c2b37ace1632304f7d97995143948145
rajatrajan03 <rajatagnihotri03@outlook.com>
2026-06-24 10:50:59 +0530

    Add Person and Student classes with inheritance and methods

--- pattern.py
+++ pattern.py
@@ -236,14 +236,25 @@
 # emp = Employee(50000)
 # print("After bonus:", emp.bonus(5000))
 
-# Q.4)Create a class Animal and inherit it to create a Dog class.
-class Animal:
-    def eat(self):
-        print("Animal is eating.")
-class Dog(Animal):
-    def bark(self):
-        print("Dog is barking.")
-   
-d=Dog()
-d.eat()
-d.bark()
+# # Q.4)Create a class Animal and inherit it to create a Dog class.
+# class Animal:
+#     def eat(self):
+#         print("Animal is eating.")
+# class Dog(Animal):
+#     def bark(self):
+#         print("Dog is barking.")       
+# d=Dog()
+# d.eat()
+# d.bark()
+
+# Q.5 Create a class Person and inherit it in students
+class Person:
+    def display(self):
+        print("This is a person.")
+        
+class Student(Person):
+    def study(self):
+        print("This is a student.")
+s = Student()
+s.display()
+s.study()
